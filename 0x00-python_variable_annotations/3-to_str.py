9f12d1c45ff0af656e242f5c04f55360f476b638
EnGentech <engen.inyang@gmail.com>
2023-10-05 20:45:06 +0100

    returned a str representation of the annotation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+A  type-annotated function to_str which
+takes a float n as argument and returns
+ float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This function takes a single argument as float,
+    convert the float into math.floor and returns
+    an integer data type
+    """
+    return str(n)
