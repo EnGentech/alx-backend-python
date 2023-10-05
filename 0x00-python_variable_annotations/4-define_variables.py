d523560bc6b154670d37b42a0c7e67531f7b6f63
EnGentech <engen.inyang@gmail.com>
2023-10-05 21:00:15 +0100

    created variables with annotation

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Author: Iberedem Inyang [EnGentech]
+This file will annotate variables
+using the annotation concept
+"""
+
+
+a: int = 1
+pi: float = 3.142
+i_understand_annotations: bool = True
+school: str = 'Holberton'
