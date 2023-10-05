1869ccd265863eda49f421adf28921d1e8b42587
EnGentech <engen.inyang@gmail.com>
2023-10-05 23:59:15 +0100

    list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Author: Iberedem Inyang [EnGentech]
+This file returns a complex data annotation
+for a list dataType
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    This function takes a list of floats and
+    returns a float dataType
+    """
+    return sum(input_list)
