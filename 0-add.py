c2e72740142c0ea077dfb6198a15b0f964961de9
EnGentech <engen.inyang@gmail.com>
2023-10-05 10:16:50 +0100

    anotating a function with floating point values

---
+++ 0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A program to anotate a function given two
+input and an output all in float data type
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    this function returns an addition
+    of two floating point numbers
+    """
+    return a + b
---
+++ README.md
@@ -0,0 +1,3 @@
+This directory is basically about python backend scripts with ALX SE
+We are commited to doing hard things and to bring out the full potentials
+of individual in your area of pursuit
