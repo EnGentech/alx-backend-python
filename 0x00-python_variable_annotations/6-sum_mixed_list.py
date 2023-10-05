e35c01fea36deaa5812906cc2f31b347d4cbf2e7
EnGentech <engen.inyang@gmail.com>
2023-10-06 00:19:08 +0100

    bugs

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,6 +5,7 @@ This file returns a complex data annotation
 for a list dataType
 """
 from typing import List, Union
+'''from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
@@ -12,4 +13,10 @@ def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     This function takes a list of floats and
     returns a float dataType
     """
-    return sum(mxd_lst)
+    return sum(mxd_lst)'''
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    '''Computes the sum of a list of integers and floating-point numbers.
+    '''
+    return float(sum(mxd_lst))
