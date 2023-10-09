48faa0140138261d865769b313114d8b1f31ec0d
EnGentech <engen.inyang@gmail.com>
2023-10-09 14:57:42 +0100

    created a basic function that operated on asyncio

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Author: Iberedem Inyang
+Description: This is a basic python function that calls
+an asyncio run function to execute a previous function
+for a given specifics
+"""
+
+
+import asyncio
+from time import time
+
+
+get = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """A basic time measure function"""
+
+    begin = time()
+    asyncio.run(get(n, max_delay))
+    end = time()
+    return (end - begin) / n
