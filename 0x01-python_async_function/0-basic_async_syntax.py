9d84340263010dfb587b533f533516414d30efc3
EnGentech <engen.inyang@gmail.com>
2023-10-09 13:35:04 +0100

    corrected bugs

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
+"""
+This file creates an asynchronous content that
+returns a random await time for the asyncio module
+"""
 
 import asyncio
 from random import uniform
 
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay=10):
+    """An asyncio function to return a random
+    delay second before a value is printed"""
+
     wait_random = uniform(0, max_delay)
     await asyncio.sleep(wait_random)
     return wait_random
