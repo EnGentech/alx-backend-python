986a170005578bd1bad7a2cf6194fa0f9299a12f
EnGentech <engen.inyang@gmail.com>
2023-10-09 15:34:08 +0100

    return asyncio_task as datatype class

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Author: Iberedem Inyang
+Description: this code defines an ordinary function
+which in turn returns a type of asyncio task function
+"""
+
+
+import asyncio
+
+
+get = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> float:
+    """A basic function that returns asyncio_task class"""
+
+    return asyncio.create_task(get(max_delay))
