5aa26e68bed86127732acf93a2e2d3955548efa6
EnGentech <engen.inyang@gmail.com>
2023-10-10 18:44:53 +0100

    asyncio iteration with comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Author: Iberedem Inyang
+Description: This is an asyncio loop function
+that will loop through and print a file for 10 times
+"""
+
+
+import asyncio
+
+
+get = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> float:
+    """loop through a data using asyncio module"""
+    return [_ async for _ in get()]
