7d382c6a6e81f925afbcaaedf8034d33c9fe6bf2
EnGentech <engen.inyang@gmail.com>
2023-10-30 10:19:06 +0100

    test this

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -27,13 +27,16 @@ class TestGithubOrgClient(unittest.TestCase):
 
 
     def test_public_repos_url(self):
-        """test"""
-        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock:
-            json_dict = {'repos_url': 'hello world'}
-            mock.return_value = json_dict
-            test = GithubOrgClient('check')
-            response = test._public_repos_url
-            self.assertEqual(response, json_dict['repos_url'])
+        """ Test that the result of _public_repos_url is the expected one
+        based on the mocked payload
+        """
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock:
+            payload = {"repos_url": "World"}
+            mock.return_value = payload
+            test_class = GithubOrgClient('test')
+            result = test_class._public_repos_url
+            self.assertEqual(result, payload["repos_url"])
 
 
 if __name__ == '__main__':
